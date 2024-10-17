e5795d7e52b9af3423f9897eeaa0219c97b8e50f
Andres Crane <andres.crane@gmail.com>
2024-10-17 12:42:37 -0500

    Split import and int conversion steps into two separate python scripts, separated code into functions

---
+++ Convert emails to ints.py
@@ -0,0 +1,81 @@
+# This script converts Wordle results to integer representation for downstream analysis
+
+# Import necessary libraries
+import json
+import numpy as np
+import pickle
+
+# Load imported emails
+with open('imported_email_data.json', 'r') as file:
+    input_data_dict = json.load(file)
+
+# Def function to find the highest puzzle number completed
+def max_puzzle_num(input_data_dict):
+
+    # Collect puzzle numbers (1st level dict keys) in list
+    puzzle_nums_list = set().union(*input_data_dict.values())
+
+    # Calculate and return max puzzle number
+    return int(max(puzzle_nums_list))
+
+
+# Def function to convert each imported email to int representation
+def convert_emails_to_int(input_data_dict):
+
+
+    # Loop through each sender email address in the data
+    for sender in input_data_dict:
+
+        # Create array of zeros to hold results
+        wordle_int_arr = np.zeros([6,5,max_puzzle_num(input_data_dict)+1], dtype='uint8')
+
+        # For each wordle_puzz puzzle, turn str into int
+        for wordle_puzz in input_data_dict[sender]:
+
+            # This wordle_puzz's guesses
+            guesses = input_data_dict[sender][wordle_puzz]
+            print(guesses)
+
+            # Create array of zeros to hold guesses
+            guesses_int = np.zeros(30)
+
+
+            # Convert wordle_puzz guesses to int
+            # 0 = space filler for unused guesses
+            # 1 = gray box (wrong letter, not in word) 11036
+            # 2 = yellow box (wrong letter, but is in word) 129000
+            # 3 = green box (right letter) 129001
+
+            guess_idx = 0 #keeps track of how many true guesses (not newline chars, for ex) have been converted to ints
+            for guess in guesses:
+                guess_int = ord(guess)
+                match guess_int:
+                    case 11036:
+                        guesses_int[guess_idx] = 1
+                        guess_idx += 1
+                    case 129000:
+                        guesses_int[guess_idx] = 2
+                        guess_idx += 1
+                    case 129001:
+                        guesses_int[guess_idx] = 3
+                        guess_idx += 1
+
+
+
+            # Reshape the array to the common wordle_puzz box shape
+            guesses_int = guesses_int.astype(np.uint8).reshape(6,5)
+
+            # Save int representation into 3-d array
+            wordle_int_arr[:,:,int(wordle_puzz)] = guesses_int
+
+            print("guesses_int: \n\n",guesses_int)
+
+        # Save int representation into dictionary with key "puzzle_as_int"
+        input_data_dict[sender]['0'] = wordle_int_arr
+
+convert_emails_to_int(input_data_dict)
+
+
+# Save int representations as pickle file
+with open('Imported_email_data_with_ints.pkl','wb') as file:
+    pickle.dump(input_data_dict, file)
--- Import email data.py
+++ Import email data.py
@@ -1,95 +1,105 @@
-# This script imports emails (.eml) downloaded from gmail containing wordle results. Wordle results from each email are stored as integers along with the puzzle number.
+# This script imports emails (.eml) downloaded from gmail containing wordle results
 
 # Import necessary libraries
-import os
-import base64
-import numpy as np
-import pandas as pd
-#from google.colab import files
+from os import listdir
+from base64 import b64decode
+import json
 
 
-# Open wordle emails
+
+# Open wordle email directory and get list of email files
 email_list_dir = '.\Wordle emails'
-email_list = os.listdir(email_list_dir)
+email_list = listdir(email_list_dir)
 
 
-## Extract wordle data from each email
+# Def function to import wordle data from each email
+def import_wordle_emails(email_list):
 
-# Create array to hold results
-guesses_int_arr = np.zeros([6,5,2000], dtype='uint8')
+  # Create dictionary to hold results
+  wordle_result_dict = {}
 
-# For each email in the upload
-for email in email_list:
-  # Open the file
-  f = open(email_list_dir + '\\' + email, 'r',encoding='utf-8')
+  # For each email in the upload
+  for email in email_list:
+    # Open the file
+    f = open(email_list_dir + '\\' + email, 'r',encoding='utf-8')
 
-  # Get the body of the email in str format
-  body = f.read()
-  #body = body.decode('UTF-8')
+    # Get the body of the email in str format
+    body = f.read()
+    #body = body.decode('UTF-8')
 
-  # Find the sender of the email (email address)
-  senderStart = body.find('From: ') + 6
-  senderEnd = body.find('\r', senderStart)
-  sender = body[senderStart:senderEnd]
-  senderEmail = sender[sender.find('<')+1 : sender.find('>')]
+    # Get the wordle results
+    wordleStart = body.find('base64')+6
+    wordleEnd = body.find('--' or 'on', wordleStart) - 1
+    wordleResult = body[wordleStart:wordleEnd]
+    
+    # Decode the wordle result and store in variable
+    wordleResult = b64decode(wordleResult).decode('UTF-8')
 
-  # Get the wordle results
-  wordleStart = body.find('base64')+6
-  wordleEnd = body.find('--', wordleStart) - 1
-  wordleResult = body[wordleStart:wordleEnd]
+    # Find the sender of the email (email address)
+    senderStart = body.find('From: ') + 6
+    senderEnd = body.find('To:', senderStart)
+    sender = body[senderStart:senderEnd]
+    senderEmail = sender[sender.find('<')+1 : sender.find('>')]  
 
-  # Decode the wordle result and store in variable
-  wordleResult = base64.b64decode(wordleResult).decode('UTF-8')
-  #print(wordleResult)
+    # Find the wordle puzzle number and save as int
+    puzzleNumber = wordleResult[7:12]
+    puzzleNumber = int(puzzleNumber.replace(',',''))
+    print(puzzleNumber)
+
+    def store_wordle_in_dict(wordleResult):
+    # Separately save wordle results for each email address
+    # as nested dictionaries
+      if senderEmail not in wordle_result_dict:
+        wordle_result_dict[senderEmail] = {puzzleNumber: wordleResult}
+      else:
+        wordle_result_dict[senderEmail][puzzleNumber] = wordleResult
+
+    # If message contains replies, split wordleResult and analyze each
+    if body.find('In-Reply-To:') > -1:  # Message contains replies
+
+      next_email_start = wordleResult.find('On ')
+      wordleResult1 = wordleResult[:next_email_start]
+      # Save
+      store_wordle_in_dict(wordleResult1)
+
+      wordleResult2 = wordleResult[next_email_start:]
+
+      # Find the email address of the reply email
+      senderStart = wordleResult2.find('<')
+      senderEnd = wordleResult2.find('>', senderStart)+1
+      sender = wordleResult2[senderStart:senderEnd]
+      senderEmail = sender[sender.find('<')+1 : sender.find('>')]  
+
+      # Find beginning of wordle puzzle
+      puzzle_start = wordleResult2.find("Wordle")
+      wordleResult2 = wordleResult2[puzzle_start:]
+
+      # Save
+      store_wordle_in_dict(wordleResult2)
+
+    else:
+      # Save
+      store_wordle_in_dict(wordleResult)
+
+    # Close the file
+    f.close()
+
+  return wordle_result_dict
+
+# Use the import_wordle_emails function to import relevant email data
+wordle_result_dict = import_wordle_emails(email_list)
+
+#print(wordle_result_dict)
+
+# Save extracted email data as json file
+with open("imported_email_data.json", "w") as outfile: 
+    json.dump(wordle_result_dict, outfile, indent=4)
 
 
 
+  
 
-
-  # Extract wordle number to int
-  puzzleNumber = wordleResult[7:12]
-  puzzleNumber = int(puzzleNumber.replace(',',''))
-  print(puzzleNumber)
-
-  # Get wordle guesses in string
-  guesses = wordleResult[17:]
-  print("Guesses: ", guesses)
-
-  # Create array of zeros to hold guesses
-  guesses_int = np.zeros(30)
-
-  # Convert wordle guesses to int
-  # 0 = space filler for unused guesses
-  # 1 = gray box (wrong letter, not in word) 11036
-  # 2 = yellow box (wrong letter, but is in word) 129000
-  # 3 = green box (right letter) 129001
-
-  guess_idx = 0 #keeps track of how many true guesses (not newline chars, for ex) have been converted to ints
-  for guess in guesses:
-    guess_int = ord(guess)
-    match guess_int:
-      case 11036:
-        guesses_int[guess_idx] = 1
-        guess_idx += 1
-      case 129000:
-        guesses_int[guess_idx] = 2
-        guess_idx += 1
-      case 129001:
-        guesses_int[guess_idx] = 3
-        guess_idx += 1
+  
 
 
 
-  # Reshape the array to the common wordle box shape
-  guesses_int = guesses_int.astype(np.uint8).reshape(6,5)
-
-  # Save int representation into 3-d array
-  guesses_int_arr[:,:,puzzleNumber] = guesses_int
-
-  print("guesses_int: \n\n",guesses_int)
-
-  # Close the file
-  f.close()
-
-# Save int representations as numpy file
-np.save('Wordle guesses as ints',guesses_int_arr)
